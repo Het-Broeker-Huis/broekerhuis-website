de572260ac3d5acceb8540d69d49a96f1151869d
Maurits Meester <mmeester@users.noreply.github.com>
2025-09-29 15:15:27 +0200

    feat(cards): add event card and three column layout to the mix

---
+++ app/components/content/EventCard.vue
@@ -0,0 +1,53 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="event-card">
+    <div class="event-card__head">
+      <slot name="head" mdc-unwrap="p" />
+    </div>
+    <div class="event-card__body">
+      <slot />
+    </div>
+    <div class="event-card__footer">
+      <slot name="footer" />
+    </div>
+  </div>
+</template>
+
+<style>
+.event-card {
+
+  &__head {
+    >img {
+      display: block;
+      width: 100%;
+    }
+  }
+
+  &__body,
+  &__footer {
+    padding-inline: calc(100vw / 24 * 1);
+  }
+
+  &__body {
+    padding-block: 1rem;
+
+    h2,
+    h3,
+    h4,
+    h5 {
+      margin-block-end: 0.5rem;
+
+      >strong {
+        display: block
+      }
+    }
+  }
+
+  &__footer {
+    display: flex;
+  }
+}
+</style>
